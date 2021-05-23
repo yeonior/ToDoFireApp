d2e4fdf1fbf849283456aed9c336f2413b7c52f5
Rus <de.mr44@gmail.com>
2021-05-23 15:09:09 +0500

    Firebase import

---
+++ ToDoFire/Controllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by ruslan on 22.05.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+}
+
---
+++ ToDoFire/Controllers/TasksViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TasksViewController.swift
+//  ToDoFire
+//
+//  Created by ruslan on 22.05.2021.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+}
