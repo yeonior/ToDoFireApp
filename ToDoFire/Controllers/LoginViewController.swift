623618a42f5b5cd66ba9b3c00b9e1e29b64720f5
Rus <de.mr44@gmail.com>
2021-05-26 15:47:56 +0500

    Interface update

--- ToDoFire/Controllers/LoginViewController.swift
+++ ToDoFire/Controllers/LoginViewController.swift
@@ -9,13 +9,37 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        warningLabel.isHidden = true
+        loginButton.layer.cornerRadius = 20
+        
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillShowNotification {
+            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        } else {
+            scrollView.contentInset = .zero
+        }
+
+        scrollView.scrollIndicatorInsets = scrollView.contentInset
     }
 
     @IBAction func loginTapped(_ sender: UIButton) {
